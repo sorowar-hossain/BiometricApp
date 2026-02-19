14bbe1ad7f35e5fdc45e78e33254e25187f91269
MD. ANAYTUL ISLAM ARPON <anaytularpon@gmail.com>
2026-02-19 11:07:19 +0600

    Add project files.

---
+++ BiometricApp/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace BiometricApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "BiometricApp" };
+        }
+    }
+}
---
+++ BiometricApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BiometricApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
