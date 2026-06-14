e2b19b4d7b4cc8b2df6cf2fb09bc44f29ecae823
Md. Ariful <md.ariful@hotmail.com>
2026-06-14 14:28:19 +0600

    ok

--- BiometricApp/App.xaml.cs
+++ BiometricApp/App.xaml.cs
@@ -30,26 +30,35 @@ namespace BiometricApp
         private void MoveToSecondMonitor(Window window)
         {
             var mauiWindow = window.Handler.PlatformView;
-
             var hwnd = WindowNative.GetWindowHandle(mauiWindow);
-
             var windowId = Win32Interop.GetWindowIdFromWindow(hwnd);
 
             var appWindow = AppWindow.GetFromWindowId(windowId);
 
             var displays = DisplayArea.FindAll();
 
-            if (displays.Count > 1)
-            {
-                var second = displays[1].WorkArea;
+            if (displays.Count <= 1)
+                return;
 
-                appWindow.MoveAndResize(new RectInt32(
-                    second.X,
-                    second.Y,
-                    second.Width,
-                    second.Height
-                ));
-            }
+            // Get current display
+            var currentDisplay = DisplayArea.GetFromWindowId(windowId, DisplayAreaFallback.Primary);
+
+            // Pick a different display (external monitor)
+            var target = displays.FirstOrDefault(d =>
+                d != currentDisplay &&
+                d.DisplayId.Value != currentDisplay.DisplayId.Value);
+
+            if (target == null)
+                return;
+
+            var workArea = target.WorkArea;
+
+            appWindow.MoveAndResize(new RectInt32(
+                workArea.X,
+                workArea.Y,
+                workArea.Width,
+                workArea.Height
+            ));
         }
 #endif
     }
